8c26b4d67201bcff2e2ee108442d01c7bd8c3eee
mythreya <mythreya.jl@gmail.com>
2018-08-12 13:41:38 -0700

    Adding a stable sliding window approach

--- sliding_window.py
+++ sliding_window.py
@@ -1,5 +1,6 @@
 from glob import glob
 import cv2
+import numpy as np
 
 
 def draw_boxes(img, boxes, color=(255, 0, 0), thickness=2):
@@ -9,9 +10,35 @@ def draw_boxes(img, boxes, color=(255, 0, 0), thickness=2):
     return img
 
 
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    size = img.shape
+    if not x_start_stop[0]:
+        x_start_stop[0] = 0
+    if not x_start_stop[1]:
+        x_start_stop[1] = img.shape[0]
+    if not y_start_stop[0]:
+        y_start_stop[0] = 0
+    if not y_start_stop[1]:
+        y_start_stop[1] = img.shape[1]
+
+    x_stride = np.int(xy_window[0] * (1 - xy_overlap[0]))
+    y_stride = np.int(xy_window[1] * (1 - xy_overlap[1]))
+
+    X_indices = [x * x_stride for x in range(np.int(x_start_stop[1]/x_stride)-1)]
+    Y_indices = [y * y_stride for y in range(np.int(y_start_stop[1]/y_stride)-1)]
+
+    boxes = []
+    for x in X_indices:
+        for y in Y_indices:
+            boxes.append(((y, x), (y+xy_window[1], x+xy_window[0])))
+
+    return boxes
+
 
 img = cv2.imread("./test_images/test1.jpg")
-boxes = (((100, 100), (200, 200)), ((300, 500), (400, 700)), ((1000, 100), (1280, 300)))
-img = draw_boxes(img, boxes)
-cv2.imshow('boxes', img)
+boxes = slide_window(img)
+overlaid = draw_boxes(img, boxes)
+
+cv2.imshow('boxes', overlaid)
 cv2.waitKey(0)
